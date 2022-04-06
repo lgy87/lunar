fd3c854774e30a7c17e015360a9526ccee66043b
Guangyao Li <lgy87@foxmail.com>
2022-04-07 00:11:37 +0800

    chore: introduce prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+}
