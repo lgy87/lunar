1bcaadf719793c78241c95fa092b1e4ce7ce35c4
Guangyao Li <lgy87@foxmail.com>
2022-04-12 00:26:28 +0800

    feat: support uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,19 @@
+import { uglify } from "rollup-plugin-uglify"
+
 export default {
   input: "./src/index",
+  plugins: [
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+      },
+    }),
+  ],
   output: {
     name: "lunar",
-    file: "./dist/lunar.js",
+    file: "./dist/index.min.js",
     format: "umd",
   },
 }
